907566d7d199af7c9c272936894ddbeb85529851
Greg Thomson <gregthomson@outlook.com>
2019-03-11 14:37:29 +0000

    adding in API training

--- python/Basics/api/basic_api.py
+++ python/Basics/api/basic_api.py
@@ -1,5 +1,45 @@
 import requests
+import json
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
+if __name__ == '__main__':
 
-print (response.status_code)
+    response = requests.get("http://api.open-notify.org/iss-pass.json")
+    print(response.status_code)
+
+    # Set up the parameters we want to pass to the API.
+    # This is the latitude and longitude of New York City.
+    parameters = {"lat": 40.71, "lon": -74 }
+
+    response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+
+    #print (response.content)
+
+    # This gets the same data as the command above
+    response = requests.get("http://api.open-notify.org/iss-pass.json?lat=40.71&lon=-74")
+    #print(response.content)
+
+
+    # This gets the same data as the command above
+    response = requests.get("http://api.open-notify.org/iss-pass.json?lat=40.71&lon=-74")
+    data = (response.json())
+
+    # Get the response from the API endpoint.
+    response = requests.get("http://api.open-notify.org/astros.json")
+    data = response.json()
+
+    names = []
+    print (data)
+
+    for y in (data["people"]):
+        print ((y['name']))
+        names.append((y['name']))
+
+
+
+
+    print (names)
+
+
+
+
+
